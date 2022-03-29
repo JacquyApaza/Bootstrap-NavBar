61956bbae5ba65a6bf8e7cfc9d2b41563070bca7
JacquyApaza <100846425+JacquyApaza@users.noreply.github.com>
2022-03-29 15:57:28 -0300

    Add files via upload

---
+++ js/Scripts.js
@@ -0,0 +1,29 @@
+﻿                        
+
+// Funciones ------------------------------------------------------------------------------------------------------------------------------------
+function Collapse( nOpcion ) {
+	$( "#DivRosario01" ).collapse( "hide" );
+	$( "#DivRosario02" ).collapse( "hide" );
+	$( "#DivRosario03" ).collapse( "hide" );
+		
+ 	$( "#NavRosario01" ).removeClass( 'active' );
+ 	$( "#NavRosario02" ).removeClass( 'active' );
+ 	$( "#NavRosario03" ).removeClass( 'active' );
+ 	
+	switch ( nOpcion ) {
+    	case 1:
+			$( "#DivRosario01" ).collapse( "show" );
+    		$( "#NavRosario01" ).addClass( "active" );	
+    		
+			break;
+		case 2:
+			$( "#DivRosario02" ).collapse( "show" );
+    		$( "#NavRosario02" ).addClass( "active" );	
+		    
+			break;
+		case 3:
+			$( "#DivRosario03" ).collapse( "show" );
+		    $( "#NavRosario03" ).addClass( "active" );		    
+	}				
+}			
+
